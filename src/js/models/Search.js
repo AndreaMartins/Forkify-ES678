1c260e651d980e8bf3e165307b78ac500512cffb
Andrea Martins <marlit8@gmail.com>
2018-04-22 15:33:20 +0200

    search

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,21 @@
-export default 'I am exported string';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+
+    this.query = query
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '2794ebef5534dfba1643d8146616387f';
+    try {
+      const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      alert(error);
+    };
+  }
+
+};
