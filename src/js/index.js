43f1e2d9aecce425ab577f33032fdf8ea6a439c6
Andrea Martins <marlit8@gmail.com>
2018-04-22 15:12:10 +0200

    api calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,21 @@
-// Global app controller
+import axios from 'axios';
 
-import num from './test';
-console.log(`I imported ${num} from another module`);
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '2794ebef5534dfba1643d8146616387f';
+  try{
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error){
+    alert(error);
+  }
+  ;
+}
+
+getResults('pizza');
+
+
+
+//2794ebef5534dfba1643d8146616387f
+// http://food2fork.com/api/search
