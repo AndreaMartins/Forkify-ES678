a0320ecb1f63de47144518a9ecaf98bf9181d6af
Andrea Martins <marlit8@gmail.com>
2018-10-03 12:17:21 +0200

    local storage implemented

--- src/js/index.js
+++ src/js/index.js
@@ -147,9 +147,6 @@ elements.shopping.addEventListener('click', e =>{
 /**
  * LIKE CONTROLLER
  */
-//TESTING
-state.likes =new Likes();
-likesView.toogleLikeMenu(state.likes.getNumLikes());
 
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
@@ -177,10 +174,24 @@ const controlLike = () => {
     likesView.toogleLikeBtn(false);
     //Remove like from UI list
     likesView.deleteLike(currentID);
-    
+
   }
   likesView.toogleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load',() => {
+  state.likes = new Likes();
+
+  //Restore likes
+  state.likes.readStorage();
+
+  //Toggle like menu button
+  likesView.toogleLikeMenu(state.likes.getNumLikes());
+
+  //Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 //Handling recipe button clicks
 elements.recipe.addEventListener('click', e =>{
